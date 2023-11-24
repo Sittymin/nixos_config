1ad90a24af98414f119910672341a9c591b93993
Sittymin <2890108976@qq.com>
2023-11-24 14:39:47 +0800

    去除镜像与WSL支持

--- flake.nix
+++ flake.nix
@@ -3,22 +3,22 @@
 
 
   # 使用镜像
-  nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
-    substituters = [
-      # replace official cache with a mirror located in China
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://cache.nixos.org/"
-    ];
+  # nixConfig = {
+  #   experimental-features = [ "nix-command" "flakes" ];
+  #   substituters = [
+  #     # replace official cache with a mirror located in China
+  #     "https://mirrors.ustc.edu.cn/nix-channels/store"
+  #     "https://cache.nixos.org/"
+  #   ];
 
-    # nix community's cache server
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
+  #   # nix community's cache server
+  #   extra-substituters = [
+  #     "https://nix-community.cachix.org"
+  #   ];
+  #   extra-trusted-public-keys = [
+  #     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  #   ];
+  # };
 
 
 
@@ -33,8 +33,8 @@
 
 
     # WSL设置
-    nixos-wsl.url = "github:nix-community/nixos-wsl";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    # nixos-wsl.url = "github:nix-community/nixos-wsl";
+    # nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, nixos-wsl,home-manager, ... }@inputs: {
